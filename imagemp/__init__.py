d3dad625d3d8a7136e5db4cb55d7c72d41fabb54
Max Nikitchenko <nikitchmtech@gmail.com>
2018-09-19 23:37:49 -0400

    Removed the imports from the .process_runners.examples folder in the package-level __init__.py file. The imported modules serve as examples and rely on the installed opencv (cv2), which shouldn't be a requirement at the package level.

--- imagemp/__init__.py
+++ imagemp/__init__.py
@@ -5,7 +5,7 @@ from .process_runners.frame_grabber import FrameGrabberRunner
 from .shared_frames.single_frame import SharedSingleFrame
 from .shared_frames.list_frames import SharedFrameList
 from .process_runners.examples.simple_display import Consumer
-from .process_runners.examples.simple_display import SimpleDisplay
-from .process_runners.examples.recorder import Recorder
+# from .process_runners.examples.simple_display import SimpleDisplay
+# from .process_runners.examples.recorder import Recorder
 
 __version__ = "0.1.1"
