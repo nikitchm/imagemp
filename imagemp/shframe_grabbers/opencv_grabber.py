b7ec7bf505bf771ee422ff116707eb4a0512455f
Max Nikitchenko <nikitchmtech@gmail.com>
2018-09-22 23:45:25 -0400

    * Turned FrameGrabberCV2File into a child of FrameGrabberCV2 -- a potentially more general class. At the moment they overlap, but another new class, FrameGrabberCV2Camera, differs from them in the way how the timestamps are defined (as well as some file frame-related functions. * commenting * refactoring

--- imagemp/shframe_grabbers/opencv_grabber.py
+++ imagemp/shframe_grabbers/opencv_grabber.py
@@ -1,12 +1,14 @@
 from __future__ import print_function
 import cv2
 from .abstract import *
+import time
 
 
-class FrameGrabberCV2File(FrameGrabberAbstract):
-    def __init__(self, vid_filename, init_unpickable=True, **kwargs):
-        super(FrameGrabberCV2File, self).__init__()
-        self.filename = vid_filename
+class FrameGrabberCV2(FrameGrabberAbstract):
+    def __init__(self, file_device_name=0, init_unpickable=True, **kwargs):
+        super(FrameGrabberCV2, self).__init__()
+        # file_device_name corresponds to the argument of the cv2.VideoCapture() method -- an integer or string
+        self.file_device_name = file_device_name
         self.timestamp = -1             # last timestamp value
         self.init_timestamp_method()
         self.iframe = None
@@ -16,17 +18,19 @@ class FrameGrabberCV2File(FrameGrabberAbstract):
             self.init_unpickable()
 
     def init_unpickable(self):
-        # self.cap = cv2.VideoCapture(self.filename)
+        # self.cap = cv2.VideoCapture(self.file_device_name)
         self.cap = self.gen_capture_obj()
-        self.is_opened = self.open_file_for_capture(self.filename)
+        self.is_opened = self.open_for_capture(self.file_device_name)
 
     def gen_capture_obj(self):
-        return cv2.VideoCapture()
-
-    def open_file_for_capture(self, filename):
         # split the generation with opening of the file to allow
         # for more versatile inheritance scenarios
-        return self.cap.open(filename=filename)
+        return cv2.VideoCapture()
+
+    def open_for_capture(self, file_device_name):
+        # file_device_name -- either a string with the path to a video file or
+        # a camera ID (0,1,2,.. -- internal for opencv)
+        return self.cap.open(file_device_name)
 
     @staticmethod
     def get_opencv_prop(obj, prop):
@@ -44,10 +48,10 @@ class FrameGrabberCV2File(FrameGrabberAbstract):
         return val
 
     def frame_i(self):
-        return int(self.get_opencv_prop(self.cap, 'CAP_PROP_POS_FRAMES'))
+        return int(self.get_opencv_prop(self.cap, 'CAP_PROP_POS_FRAMES'))   # returns -1 if a camera
 
     def frame_ms(self):
-        return self.get_opencv_prop(self.cap, 'CAP_PROP_POS_MSEC')
+        return self.get_opencv_prop(self.cap, 'CAP_PROP_POS_MSEC')   # returns -1 if a camera
 
     def get_timestamp(self):
         return self.frame_ms()
@@ -58,26 +62,29 @@ class FrameGrabberCV2File(FrameGrabberAbstract):
 
     def capture(self, shared_frame=None):
         # If shared_frame is not None, capture and write the next frame
-        # (im and timestamp) into the shared_frame,
-        # otherwise, return the next frame and the timestamp
-        ret = False
+        # (im and timestamp) into the shared_frame, otherwise, return the
+        # next frame and timestamp (this way, the captured frame can be written
+        # directly into the shared memory, avoiding an intermediate memory copy)
+        _success = False
         if self.cap.isOpened():
             if shared_frame is not None:
                 try:
                     # ret, shared_frame.im[:] = self.cap.read()
                     # t = time.time()
-                    ret, shared_frame.im = self.cap.read()
+                    _success, shared_frame.im = self.cap.read()
+                    # if not _success:
+                    #     raise NameError("Couldn't read the frame.")
                     # ret, shared_frame.im[:] = self.cap.read()
                     # ret = self.cap.read(shared_frame.im[:])
                     # self.n += 1
                     # self.dtmean = (self.dtmean * (self.n-1) + time.time()-t) / self.n
                     # print('dt={}'.format(self.dtmean))
                 except Exception as e:
-                    pass
-                if ret:
+                    print('Error while trying to read the frame: {}'.format(e))
+                if _success:
                     self.timestamp = self.get_timestamp()
                     shared_frame.timestamp = self.timestamp
-                return ret
+                return _success
             else:
                 # Return the next captured frame
                 return self.cap.read(), self.get_timestamp()
@@ -87,3 +94,31 @@ class FrameGrabberCV2File(FrameGrabberAbstract):
             self.cap.release()
         finally:
             self.cap = None
+
+
+class FrameGrabberCV2File(FrameGrabberCV2):
+    def __init__(self, vid_filename, init_unpickable=True, **kwargs):
+        super(FrameGrabberCV2File, self).__init__(file_device_name=vid_filename, init_unpickable=init_unpickable, **kwargs)
+
+    def frame_i(self):
+        return int(self.get_opencv_prop(self.cap, 'CAP_PROP_POS_FRAMES'))
+
+    def frame_ms(self):
+        return self.get_opencv_prop(self.cap, 'CAP_PROP_POS_MSEC')
+
+    def get_timestamp(self):
+        return self.frame_ms()
+
+
+class FrameGrabberCV2Camera(FrameGrabberCV2):
+    def __init__(self, device_id, init_unpickable=True, **kwargs):
+        super(FrameGrabberCV2Camera, self).__init__(file_device_name=device_id, init_unpickable=init_unpickable, **kwargs)
+
+    def frame_i(self):
+        return -1
+
+    def frame_ms(self):
+        return -1
+
+    def get_timestamp(self):
+        return time.time()
