a66085669b061f4d13c28412096610adcc067526
Max Nikitchenko <nikitchmtech@gmail.com>
2018-09-22 23:33:03 -0400

    Comments + added the .run() method into the process_runners/abstract.py file, to underscore the basic idea behind it's purpose.

--- imagemp/process_runners/abstract.py
+++ imagemp/process_runners/abstract.py
@@ -1,9 +1,16 @@
 import multiprocessing as mp
 
+
+# Define default set of methods to be present in process runners (e.g., a consumer or frame-grabber).
+# Specify the termination methods (exit() and is_exiting()) which would allow the interruption of the
+# process running from another process
 class ProcessRunnerAbstract(mp.Process):
     def __init__(self):
         super(ProcessRunnerAbstract, self).__init__()
         self.exit_event = mp.Event()
+
+    def run(self):
+        pass
 
     def exit(self):
         # Triggers an exit event which should be detected in the process loop
